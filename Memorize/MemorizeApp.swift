bedfb8cafa3bf02583204e5c4b4a3dbdc8387681
Jaden Andrews <jadenandrews831@gmail.com>
2020-09-25 14:53:49 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jaden  Andrews  on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
